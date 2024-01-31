612963dc2f38c9450b8d3c5dad647f4278db746f
MuhammedHashimT <hashimt4567@gmail.com>
2024-01-31 23:18:13 +0530

    Add username and password state variables and implement basic login functionality

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,19 +1,32 @@
 "use client";
 import React from 'react'
+import Students from '../data/students.json'
 
 const Login = () => {
 
+    const [username, setUsername] = React.useState<string>("")
+    const [password, setPassword] = React.useState<string>("")
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+       
+        // check the username that matches from students data on studentId
+        // if matches then check the password is 123
+
+        const student = Students.find((student)=>student.StudentId === parseInt(username))
+        if(student){
+           
+        }
+
     }
 
   return (
     <div>
         <form onSubmit={handleSubmit} >
             <p>Username</p>
-            <input type="text" name="" id=""  />
+            <input onChange={(e)=>setUsername(e.target.value)}  type="text" name="" id=""  />
             <p>Password</p>
-            <input type="text" name="" id=""  />
+            <input onChange={(e)=>setPassword(e.target.value)}  type="text" name="" id=""  />
         </form>
     </div>
   )
