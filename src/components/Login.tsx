b8fe94a22149a8cb7aa5b4b89ff43a41bfaceab6
MuhammedHashimT <hashimt4567@gmail.com>
2024-01-31 23:35:47 +0530

    Add error handling and support for Dars and admin login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from 'react'
 import Students from '../data/students.json'
+import Dars from '../data/dars.json'
 
 const Login = () => {
 
     const [username, setUsername] = React.useState<string>("")
     const [password, setPassword] = React.useState<string>("")
+    const [error, setError] = React.useState<string>("")
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -14,8 +16,23 @@ const Login = () => {
         // if matches then check the password is 123
 
         const student = Students.find((student)=>student.StudentId === parseInt(username))
+        const dars =   Dars.find((dars)=>dars.DarsCode === username)
+        const admin = username === "JMADMIN" ? true : false
         if(student){
-           
+            password === "123" ? 
+            localStorage.setItem("user", JSON.stringify(student.StudentId)) :
+            setError("Invalid Password")
+        }else if(dars){
+            password === "123" ? 
+            localStorage.setItem("user", JSON.stringify(dars.DarsCode)) :
+            setError("Invalid Password")
+        }else if(admin){
+            password === "123" ? 
+            localStorage.setItem("user", JSON.stringify("JMADMIN")) :
+            setError("Invalid Password")
+        }
+        else{
+            setError("Invalid Username")
         }
 
     }
