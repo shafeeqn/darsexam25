70ab5f1e2a607882b54cc4dfb2a966e15f9bf0a7
new-user6663 <139827666+new-user6663@users.noreply.github.com>
2024-01-31 19:36:29 +0000

    changes in dars

--- src/app/dars/page.tsx
+++ src/app/dars/page.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import Students from "../../data/students.json";
+import Subjects from "../../data/subjects.json";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [username, setUsername] = useState<string>("");
 
-export default page
+  useEffect(() => {
+    setUsername(JSON.parse(localStorage.getItem("user") as string));
+  }, []);
+
+  const students = Students.filter((student) => student.DarsCode == username);
+  console.log(students);
+  const subjectId: any = [];
+  students.map((student) => {
+    if (!subjectId.includes(student.Subject1 as never)) {
+      subjectId.push(student.Subject1 as never);
+    }
+    if (!subjectId.includes(student.Subject2 as never)) {
+      subjectId.push(student.Subject2 as never);
+    }
+  });
+  console.log(subjectId);
+
+  const subjects = Subjects.filter((subject) => subjectId.includes(subject.Id));
+  console.log(subjects);
+  
+
+  return (
+    <div>
+      <p>{username}</p>
+      {students.map((student) => {
+        return (
+          <div className="flex gap-5">
+            <p>{student.Name}</p>
+            <p>{student.StudentId}</p>
+            <button>Download</button>
+          </div>
+        );
+      })}
+
+{subjects.map((subject) => {
+        return (
+          <div className="flex gap-5">
+            <p>{subject.Id}</p>
+            <p>{subject.Name}</p>
+            <button>Download</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default page;
