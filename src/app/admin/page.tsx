f07dcc03f7172c193f1cf7b65240bbd25b16acf4
shafeeqn <149598035+shafeeqn@users.noreply.github.com>
2025-03-15 14:08:49 +0530

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -137,9 +137,9 @@ const page = () => {
         <p className="w-40 text-center line-clamp-1 font-semibold pl-2 border-[1px] border-black text-sm">
           DarsCode
         </p>
-{/*          <p className="w-28 text-center line-clamp-1 font-semibold pl-2 border-[1px] border-black text-sm"> 
+      <p className="w-28 text-center line-clamp-1 font-semibold pl-2 border-[1px] border-black text-sm"> 
           Mark
-        </p> */}
+        </p>
       </div>
       {students
         .filter(
@@ -161,9 +161,9 @@ const page = () => {
             <p className="w-40 text-left line-clamp-1 font-semibold pl-2 border-[1px] border-black text-sm">
               {student.DarsCode}
             </p>
-{/*             <p className="w-28 text-center line-clamp-1 font-semibold pl-2 border-[1px] border-black text-sm">
+            <p className="w-28 text-center line-clamp-1 font-semibold pl-2 border-[1px] border-black text-sm">
               {student.Subject1 === selectedSubject ? student["Mark 1"] : student["Mark 2"] }
-            </p> */}
+            </p>
           </div>
         ))}
     </div>
